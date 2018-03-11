2387e818a0e2db2ffa247aecb645d72238b61811
Derek Dunagan <derek.dunagan@gmail.com>
2018-03-11 14:38:01 -0600

    fix parse error + add blurb + cleanup cli feedback

--- source/index.js
+++ source/index.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-sync */
 // eslint-disable-next-line max-params
-(function main() {
+(function main(FS) {
     const GOOGLE_MAPS_WEB_URL_PREFIX = 'https://www.google.com/maps?q=';
     const PLACE_ID = 78;
     const PLACE_TITLE = 11;
@@ -11,26 +12,33 @@
     const PLACE_COORDS = 9;
     const PLACE_COORDS_LAT = 2;
     const PLACE_COORDS_LONG = 3;
+    const PLACE_BLURB_0 = 25;
+    const PLACE_BLURB_1 = 15;
+    const PLACE_BLURB_2 = 0;
+    const PLACE_BLURB_3 = 2;
     return module.exports = parse_files_for_places;
 
     // -----------
 
-    function parse_files_for_places(files) {
+    function parse_files_for_places(file_list) {
         let places = [];
-        for (const file_contents of files) {
-            places = places.concat(parse_file_for_places(file_contents));
+        for (const file_name of file_list) {
+            const file_contents = FS.readFileSync(file_name, 'utf8');
+            places = places.concat(
+                parse_file_for_places(file_name, file_contents),
+                ); // eslint-disable-line
         }
-        console.log(places); // eslint-disable-line no-console
         return places;
     }
 
-    function parse_file_for_places(raw_file_contents) {
+    function parse_file_for_places(file_name, raw_file_contents) {
         // eslint-disable-next-line max-len
         const file_contents_object = JSON.parse(raw_file_contents.substring(0, raw_file_contents.length - 6));
         let d = file_contents_object.d.substring(5)
-            .replace(/\\"/g, '"')
+            .replace(/(?!\\)\\"/g, '"')
             .replace(/\\n/g, '')
             ; // eslint-disable-line indent
+        // FS.writeFileSync(`${ file_name }: d.txt`, d);
         d = JSON.parse(d);
         const raw_places = d[0][1];
         const places = [];
@@ -55,6 +63,12 @@
                     url: raw_meta[PLACE_WEBSITE][PLACE_WEBSITE_URL],
                     }, // eslint-disable-line indent
                 coords: [ lat, long ],
+                blurb: raw_meta[PLACE_BLURB_0]
+                    && raw_meta[PLACE_BLURB_0][PLACE_BLURB_1]
+                    && raw_meta[PLACE_BLURB_0][PLACE_BLURB_1][PLACE_BLURB_2]
+                    && raw_meta[PLACE_BLURB_0][PLACE_BLURB_1][PLACE_BLURB_2][PLACE_BLURB_3] // eslint-disable-line max-len
+                    || null
+                    , // eslint-disable-line
                 }; // eslint-disable-line indent
             const google_maps_q = encodeURIComponent(
                 `${ place.title }, ${ place.address.join(', ') }`,
@@ -65,4 +79,6 @@
             return place;
         }
     }
-}());
+}(
+    require('fs'),
+));
